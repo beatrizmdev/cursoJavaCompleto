e587b7dbef2f07b8ab20cbe0122755a97c04aec1
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-15 21:48:27 -0300

    feat: declaring variables

---
+++ ws-Eclipse/declarandoVariaveis/src/declarandoVariaveis/Main.java
@@ -0,0 +1,15 @@
+package declarandoVariaveis;
+
+public class Main {
+	public static void main(String[] args) {
+
+	
+	int idade = 22;
+	double altura = 1.63;
+	char sexo = 'F';
+	String nome = "Ana";
+	
+	System.out.println("Sou " + nome + ", tenho " + idade + " anos e " + altura + "cm de altura.");
+	
+	}
+}
