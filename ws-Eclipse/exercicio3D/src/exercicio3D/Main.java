2adacf80480970974aebadf8f9e58a33544c3f3f
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-25 22:39:27 -0300

    feat: pair division with denominator check exercise

---
+++ ws-Eclipse/exercicio3D/src/exercicio3D/Main.java
@@ -0,0 +1,26 @@
+package exercicio3D;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner entrada = new Scanner(System.in);
+		int quant = entrada.nextInt();
+		
+		for (int i=0; i<quant; i++) {
+			int num1 = entrada.nextInt();
+			int num2 = entrada.nextInt();
+			
+			if (num2 == 0) {
+				System.out.println("Divisão impossível!");
+			} else {
+				double divisao = (double) num1/num2;
+				System.out.printf("%.2f%n", divisao);				
+			}
+		}
+		
+		entrada.close();
+	}
+}
