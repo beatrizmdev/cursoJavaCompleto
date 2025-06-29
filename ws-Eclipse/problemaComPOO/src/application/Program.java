2f48d65ee54a88b426636cc6912ba7c660a6dd0b
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-28 23:41:40 -0300

    feat: class with method

--- ws-Eclipse/problemaComPOO/src/application/Program.java
+++ ws-Eclipse/problemaComPOO/src/application/Program.java
@@ -26,10 +26,8 @@ public class Program {
 		y.b = entrada.nextDouble();
 		y.c = entrada.nextDouble();
 		
-		double p = (x.a + x.b + x.c) / 2.00;
-		double areaX = Math.sqrt(p * (p-x.a) * (p-x.b) * (p-x.c));
-		p = (y.a + y.b + y.c) / 2.00;
-		double areaY = Math.sqrt(p * (p-y.a) * (p-y.b) * (p-y.c));
+		double areaX = x.area();
+		double areaY = y.area();
 		
 		System.out.printf("X triangle area: %.4f%n", areaX);
 		System.out.printf("Y triangle area: %.4f%n", areaY);
