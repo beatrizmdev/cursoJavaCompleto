de89b8b078603ef05292c9d831913d8e9c0432c9
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-07-30 18:45:17 -0300

    feat: learning about upcasting

--- ws-Eclipse/heranca/src/application/Program.java
+++ ws-Eclipse/heranca/src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		BusinessAccount account = new BusinessAccount();
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+	
+		// UPCASTING: casting de uma subclasse para superclasse
 		
-		account.deposit(200.00);
+		Account acc1 = bacc; // obj de subclasse está sendo atribuído à um objeto de superclasse
+							 // isso é possível porque a HERANÇA é uma relação "é-um".
+							 // Ou seja, uma BusinessAccount "é-um" Account.
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0); // Veja que não gera um erro
+		Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01); // SavingAccount "é-um" Account
 	}
 }
