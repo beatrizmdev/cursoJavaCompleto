190dd2c19464e21ad57a3ebc58fff3a8bf2159a0
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-07-29 21:41:50 -0300

    feat: inheritance and the protected access modifier

---
+++ ws-Eclipse/heranca/src/entities/Account.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Account {
+
+	private Integer Number;
+	private String holder;
+	protected Double balance; // um membro definido como protected pode ser acessado:
+							  // - dentro da própria classe;
+							  // - em uma classe diferente que esteja no mesmo pacote;
+							  // - em um pacote diferente contanto que seja uma subclasse.
+	
+	public Account() {
+	}
+
+	public Account(Integer number, String holder, Double balance) {
+		Number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public Integer getNumber() {
+		return Number;
+	}
+
+	public void setNumber(Integer number) {
+		Number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+		
+}
