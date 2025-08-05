f912e26fcff5827cce86e6d4214fddbdc2ad5b82
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-08-04 22:30:34 -0300

    feat: first polymorphism exercise

---
+++ ws-Eclipse/polymorphismExercise/src/entities/Employees.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class Employees {
+	
+	private String name;
+	private Integer hours;
+	private Double valuePerHour;
+	
+	public Employees() {
+	}	
+	public Employees(String name, Integer hours, Double valuePerHour) {
+		this.name = name;
+		this.hours = hours;
+		this.valuePerHour = valuePerHour;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getHours() {
+		return hours;
+	}
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+	
+	public Double payment() {
+		return valuePerHour*hours;
+	}
+}
---
+++ ws-Eclipse/polymorphismExercise/src/entities/OutsourcedEmployees.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class OutsourcedEmployees extends Employees {
+	
+	private Double additionalCharge;
+	
+	public OutsourcedEmployees() {
+	}
+	public OutsourcedEmployees(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+	
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+	@Override
+	public Double payment() {
+		return super.payment() + (1.1* additionalCharge);
+	}
+	
+	
+}
