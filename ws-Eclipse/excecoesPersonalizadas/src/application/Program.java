e5d292b4ecab7daa242649107b5ffeb082e02ffc
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-08-16 20:10:30 -0300

    feat: good solution but without personalized exceptions

--- ws-Eclipse/excecoesPersonalizadas/src/application/Program.java
+++ ws-Eclipse/excecoesPersonalizadas/src/application/Program.java
@@ -9,22 +9,23 @@ import model.entities.Reservation;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.print("Room number: ");
-		int roomNumber = sc.nextInt();
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sc.next());
-		
-		if (!checkOut.after(checkIn)) { // after é um método da classe Date que testa se uma data é depois da outra
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}
-		else {
+		try { // aqui já pode tratar a parse exception
+			
+			System.out.print("Room number: ");
+			int roomNumber = sc.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-ou t date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+			
+			// lógica ideal
+			// aqui removemos todo o código de verificação do programa principal
+	
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
 			
@@ -33,20 +34,19 @@ public class Program {
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
-			checkOut = sdf.parse(sc.next());
+			checkOut = sdf.parse(sc.next());		
 			
-			// lógica um pouco menos ruim:
-			// passamos a lógica de validação da reserva pra classe Reservation, mas ainda não é a melhor opção
-			// aqui, o método vai retornar uma String, algo mais comuns em linguagens antigas como C
-			
-			String error = reservation.updateDates(checkIn, checkOut);
-			if (error != null) {
-				System.out.println("Error in reservation: " + error);
-			}
-			else {
-				System.out.println("Reservation: " + reservation);
-			}
-		}		
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		
+		}
+		// qualquer erro, cairá nos blocos catch's
+		catch (ParseException error) {
+			System.out.println("Invalid date format");
+		}
+		catch(IllegalArgumentException error) {
+			System.out.println("Error in reservation: " + error.getMessage());
+		}
 		
 		sc.close();
 	}
--- ws-Eclipse/excecoesPersonalizadas/src/model/entities/Reservation.java
+++ ws-Eclipse/excecoesPersonalizadas/src/model/entities/Reservation.java
@@ -39,17 +39,16 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // converte de milisegundos pra dias
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) { // não vamos mais enviar uma String, vamos realmente lançar uma exceção
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates";
+			throw new IllegalArgumentException("Reservation dates for update must be future dates");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Check-out date must be after check-in date";
+			throw new IllegalArgumentException("Check-out date must be after check-in date");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null; // retornará nulo se n der nenhum erro
 	}
 	
 	@Override
