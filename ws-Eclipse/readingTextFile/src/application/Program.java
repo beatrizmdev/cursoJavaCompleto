9b3d27696e2a38b842466b4539d8f80706db3cf2
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-08-19 21:49:33 -0300

    feat: reading text file with File and Scanner classes

---
+++ ws-Eclipse/readingTextFile/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\Users\\abbm9\\OneDrive\\Documentos\\temp\\in.txt");
+		Scanner sc = null;
+		
+		try { // o programa vai tentar abrir o arquivo e isso pode gerar um erro: IO Exception
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException error) {
+			System.out.println("Error: " + error.getMessage());
+		}
+		finally { // o scanner vai ser fechado independente se deu certo ou não
+			if (sc != null) { // s ó fecha se não tiver dado erro lá no início, na instanciação do arquivo
+				sc.close();
+			}
+		}
+	}
+
+}
