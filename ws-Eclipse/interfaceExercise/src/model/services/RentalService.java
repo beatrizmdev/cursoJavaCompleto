8e833ad261e8e562175200b726ee4324376981b4
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-08-25 23:48:36 -0300

    feat: first interface exercise - final part

--- ws-Eclipse/interfaceExercise/src/model/services/RentalService.java
+++ ws-Eclipse/interfaceExercise/src/model/services/RentalService.java
@@ -10,9 +10,9 @@ public class RentalService {
 	private Double pricePerDay;
 	private Double pricePerHour;
 	
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
@@ -28,7 +28,7 @@ public class RentalService {
 			basicPayment = pricePerHour * Math.ceil(hours);
 		}
 		else {
-			basicPayment = pricePerDay * Math.ceil(hours / 24);
+			basicPayment = pricePerDay * Math.ceil(hours/24);
 		}
 		
 		double tax = taxService.tax(basicPayment);
