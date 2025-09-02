1e4ddddf9bfde9ad83b52781c6463f651e78e46f
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-09-01 22:32:46 -0300

    feat: interface comparable

---
+++ ws-Eclipse/interfaceComparable/src/entities/Employee.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Employee implements Comparable<Employee> {
+	
+	private String name;
+	private Double salary;
+	
+	public Employee(String name, Double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int compareTo(Employee other) {
+		return -salary.compareTo(other.getSalary());
+	}
+	
+	/* por salário em ordem crescente
+	@Override
+	public int compareTo(Employee other) {
+		return salary.compareTo(other.getSalary());
+	}
+	 */
+	
+	/* por nome em ordem alfabética
+	@Override
+	public int compareTo(Employee other) {
+		return name.compareTo(other.getName());
+	}
+	 */
+	
+	
+}
