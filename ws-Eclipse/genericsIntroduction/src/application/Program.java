9055bc29bc2cfbb0e152b02cc73eb65cdf19a6a1
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-09-03 14:31:58 -0300

    feat: using Generics to ensure Type Safety and reuse of the PrintService class

--- ws-Eclipse/genericsIntroduction/src/application/Program.java
+++ ws-Eclipse/genericsIntroduction/src/application/Program.java
@@ -10,22 +10,18 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		
-		PrintService ps = new PrintService();
+		PrintService<String> ps = new PrintService<>();
 		int n = sc.nextInt();
 		
 		for (int i=0; i<n; i++) {
-			Object value = sc.next();
+			String value = sc.next();
 			ps.addValue(value);
 		}
 		
 		ps.print();
 		System.out.println();
-		System.out.println("First: " + ps.first());
-		
-		// PROBLEMA type safety: não conseguimos guardar nenhuma dessas informações de maneira direta.
-		// Para guardar essas infos, tenho que fazer um casting ("int n = ps.first()" n rola pq ta inserindo um object num int)
-		// Dentro disso não há a sgurança de tipos em, por exemplo, se eu inserir manualmente "ps.addValue("Maria"), antes do for,
-		// teremos um erro de múltiplos tipos, um acaba não podendo fazer casting pra outro, ex: String -> int.
+		String x = ps.first();
+		System.out.println("First: " + x);
 		
 		sc.close();
 	}
