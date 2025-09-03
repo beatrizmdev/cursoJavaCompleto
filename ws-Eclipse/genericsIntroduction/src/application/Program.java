1d1eab2caee564fa8baa09726186dce586262155
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-09-03 14:23:38 -0300

    feat: trying to solve with Object

--- ws-Eclipse/genericsIntroduction/src/application/Program.java
+++ ws-Eclipse/genericsIntroduction/src/application/Program.java
@@ -2,7 +2,7 @@ package application;
 
 import java.util.Scanner;
 
-import entities.PrintServiceString;
+import entities.PrintService;
 
 public class Program {
 
@@ -10,17 +10,22 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		
-		PrintServiceString ps = new PrintServiceString();
+		PrintService ps = new PrintService();
 		int n = sc.nextInt();
 		
 		for (int i=0; i<n; i++) {
-			String value = sc.next();
+			Object value = sc.next();
 			ps.addValue(value);
 		}
 		
 		ps.print();
 		System.out.println();
 		System.out.println("First: " + ps.first());
+		
+		// PROBLEMA type safety: não conseguimos guardar nenhuma dessas informações de maneira direta.
+		// Para guardar essas infos, tenho que fazer um casting ("int n = ps.first()" n rola pq ta inserindo um object num int)
+		// Dentro disso não há a sgurança de tipos em, por exemplo, se eu inserir manualmente "ps.addValue("Maria"), antes do for,
+		// teremos um erro de múltiplos tipos, um acaba não podendo fazer casting pra outro, ex: String -> int.
 		
 		sc.close();
 	}
