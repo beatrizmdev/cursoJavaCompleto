af2e2842f4d731dd5a5dfd34fc95c7c0a1b155f9
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-09-07 21:02:01 -0300

    feat: set basic methods

---
+++ ws-Eclipse/set/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		
+		// Set<String> set = LinkedHashSet<>();
+		// Set<String> set = TreeSet<>();
+		Set<String> set = new HashSet<>();
+		
+		set.add("TV");
+		set.add("Notebook");
+		set.add("Tablet");
+		
+		// set.remove("Tablet");
+		// set.removeIf(x -> x.length() >= 3);
+		set.removeIf(x -> x.charAt(0) == 'T');
+						
+		for (String p : set) {
+			System.out.println(p);
+		}
+	}
+}
