14de84dee55c6e34ec3f8625d17446851d56fddc
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-09-10 17:36:40 -0300

    feat: 4th way to use comparator, with a lambda expression sintaxe to use the Comparator

--- ws-Eclipse/comparator/src/application/Program.java
+++ ws-Eclipse/comparator/src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -15,24 +14,8 @@ public class Program {
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-		
-		// função anônima ou arrow function:
-		// (parâmetros) -> { implementação da função }
-		// nos parâmetros não precisa informar o tipo
-		
-		
-		Comparator<Product> comp = (p1, p2) -> { // faremos uma implementação de expressão lambda, criando uma função anônima
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
-		
-		/*
-		 * OBS: como a implementação tem apenas uma linha, posso também dispensar o "return" e as chaves, deixando dessa forma:
-		 * 
-		 * Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		 * 
-		 */
-
-		list.sort(comp);
+			
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		
 		for (Product p : list) {
 			System.out.println(p);
