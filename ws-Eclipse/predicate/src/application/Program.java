c253f6c3d3d27848244c0bc91c50460ebe897c58
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-09-15 21:26:48 -0300

    feat: non static method reference predicate

--- ws-Eclipse/predicate/src/application/Program.java
+++ ws-Eclipse/predicate/src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		// method reference
-		list.removeIf(Product::staticProductPredicate);
+		list.removeIf(Product::nonStaticProductPredicate);
 		
 		for (Product p : list) {
 			System.out.println(p);
