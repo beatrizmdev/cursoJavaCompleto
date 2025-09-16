2566e732e8a003052a8acd794622e82c6d830eba
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-09-16 17:47:24 -0300

    feat: consumer with inline lambda expression

--- ws-Eclipse/consumerUses/src/application/Program.java
+++ ws-Eclipse/consumerUses/src/application/Program.java
@@ -21,10 +21,8 @@ public class Program {
 		
 		double factor = 1.1;
 		
-		// expressão lambda declarada
-		Consumer<Product> cons = p -> p.setPrice(p.getPrice()*factor);
-		
-		list.forEach(cons);
+		// expressão lambda inline		
+		list.forEach(p -> p.setPrice(p.getPrice()*factor));
 		
 		list.forEach(System.out::println);
 	}
