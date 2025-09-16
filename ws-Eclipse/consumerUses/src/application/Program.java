33c695c0b12af8d76fcde4e8b3000c3244cc860a
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-09-16 17:46:27 -0300

    feat: consumer with declared lambda expression

--- ws-Eclipse/consumerUses/src/application/Program.java
+++ ws-Eclipse/consumerUses/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -18,8 +19,12 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		// reference method com método não estático
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1;
+		
+		// expressão lambda declarada
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice()*factor);
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 	}
